6bb88bdc1af600476e9712bc91bf97280ecaf73c
ravindu14 <ravindulandekumbura14@gmail.com>
2024-09-20 19:37:55 +1000

    change db link

--- book_catalog/app/database.py
+++ book_catalog/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://ravindu_sit722_postgresql_0nfp_user:Olo8Q4msmzItCh0AQVlUy1eqgzmDZnhW@dpg-crjrckjtq21c73a598h0-a.oregon-postgres.render.com/ravindu_sit722_postgresql_0nfp" #os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
